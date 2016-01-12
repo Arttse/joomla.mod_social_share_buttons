f5f7aaf2986b6f1898a20cfa2cd80ca9e68acbee
Nikita Bystrov <arttsesoft@gmail.com>
2016-01-12 17:01:50 +0300

    Initial

---
+++ mod_social_share_buttons/mod_social_share_buttons.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @copyright      Copyright (C) 2016 Nikita «Arttse» Bystrov. All rights reserved.
+ * @license        License GNU General Public License version 3
+ * @author         Nikita «Arttse» Bystrov
+ */
+
+defined ( '_JEXEC' ) or die;
+
+$moduleclass_sfx = htmlspecialchars ( $params->get ( 'moduleclass_sfx' ) );
+
+require JModuleHelper::getLayoutPath ( $module->module, $params->get ( 'layout', 'default' ) );
