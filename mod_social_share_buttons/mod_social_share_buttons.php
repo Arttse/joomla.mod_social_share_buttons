da98d33d54a02ca3d6b7d8503b4e256896fdf6f6
Nikita Bystrov <arttsesoft@gmail.com>
2016-01-13 17:38:11 +0300

    Added styles, FontAwesome and settings

--- mod_social_share_buttons/mod_social_share_buttons.php
+++ mod_social_share_buttons/mod_social_share_buttons.php
@@ -7,6 +7,25 @@
 
 defined ( '_JEXEC' ) or die;
 
+var_dump($params->get ( 'include_style-min-css', true ));
+
+if ( $params->get ( 'include_style-css', true ) )
+{
+    if ( $params->get ( 'include_style-min-css', true ) )
+    {
+        JHtml::stylesheet ( 'mod_social_share_buttons/style.min.css', false, true );
+    }
+    else
+    {
+        JHtml::stylesheet ( 'mod_social_share_buttons/style.css', false, true );
+    }
+}
+
+if ( $params->get ( 'include_font-awesome', true ) )
+{
+    JHtml::stylesheet ( 'mod_social_share_buttons/font-awesome.min.css', false, true );
+}
+
 $moduleclass_sfx = htmlspecialchars ( $params->get ( 'moduleclass_sfx' ) );
 
 require JModuleHelper::getLayoutPath ( $module->module, $params->get ( 'layout', 'default' ) );
